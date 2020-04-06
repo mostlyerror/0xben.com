1bfda7a4169414e943272b1015dd1696317046ec
Benjamin Poon <benjamintpoon@gmail.com>
2020-04-06 17:15:28 -0600

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Benjamin Poon's Portfolio`,
+    description: `Pew pew pew`,
+    author: `@mostly_error`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

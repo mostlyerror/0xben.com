fc1f038dca7fd91ae6f6c6d09fa0166a1ae2a98d
Benjamin Poon <benjamintpoon@gmail.com>
2020-04-06 17:45:15 -0600

    make video iframe wider

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import SEO from "../components/seo"
 const Video = ({ videoSrcURL, videoTitle, ...props }) => (
   <div className="video">
     <iframe
+      style={{ width: '100%'}}
       src={videoSrcURL}
       title={videoTitle}
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"

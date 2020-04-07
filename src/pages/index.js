c51e44962f8b19b973e406bba8f58e11adeb4d02
Benjamin Poon <benjamintpoon@gmail.com>
2020-04-06 19:16:34 -0600

    add base fonts to body el

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import { FaTwitter, FaGithubAlt } from "react-icons/fa"
-import './index.css'
+import "./index.css"
 
 const Video = ({ videoSrcURL, videoTitle, ...props }) => (
   <div className="videoWrapper">
@@ -26,10 +26,10 @@ const SocialButton = ({ url, text, iconComponent, children }) => (
       borderColor: "black",
       borderStyle: "solid",
       borderRadius: 5,
-      marginRight: '0.5em',
+      marginRight: "0.5em",
     }}
   >
-    <a style={{textDecoration: 'none',}} href={url}>
+    <a style={{ textDecoration: "none" }} href={url}>
       {text} {iconComponent}
     </a>
   </li>
@@ -38,14 +38,14 @@ const SocialButton = ({ url, text, iconComponent, children }) => (
 const IndexPage = () => (
   <Layout>
     <SEO title="Benjamin Poon's Portfolio" />
-    <h1 style={{ marginTop: "15vh" }}>Hi beautiful people 👋</h1>
+    <h1 style={{ marginTop: "15vh" }}>Hello beautiful people 👋</h1>
     <h1>I'm working on this page right now</h1>
     <div style={{ marginTop: "2em" }}>
       <Video
         videoSrcURL="https://media.giphy.com/media/f978c5EFp3Vu0/giphy.mp4"
         videoTitle="Kermit Typing"
       />
-      <h3 style={{marginTop: '2em'}}>Come back real soon, k?</h3>
+      <h3 style={{ marginTop: "2em" }}>Come back real soon, k?</h3>
       <ul
         style={{
           margin: 0,
